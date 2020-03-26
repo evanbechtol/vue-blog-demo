790c4bf1ffcbd281bd02c1d9434ce5f7847ff404
evan bechtol <evanbechtol@gmail.com>
2020-03-26 10:47:04 -0500

    Update Disqus comments url
    - Set to netlify url and disqus settings

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -16,8 +16,8 @@
       <section class="post__body rte" v-html="content"></section>
 
       <footer class="post__footer">
-        <vue-disqus v-if="commentsReady" shortname="vue-blog-demo"
-          :key="post" :identifier="post" :url="`https://vue-blog-demo.netlify.com/read/${post}`"/>
+        <vue-disqus v-if="commentsReady" shortname="jalenyoung"
+          :key="post" :identifier="post" :url="`https://jalen-young.netlify.com/read/${post}`"/>
       </footer>
     </article>
   </transition>
